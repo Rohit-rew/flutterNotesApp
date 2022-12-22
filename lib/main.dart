824c7f4d594784bc650b8a2553ad45c387216d22
Rohit Kumar <rohit.rew@gmail.com>
2022-12-22 10:04:28 +0530

    configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/services/Auth.dart';
-import 'package:notes/views/addNote_view.dart';
 import 'package:notes/views/login_view.dart';
 import 'package:notes/views/notes_view.dart';
 import 'package:notes/views/register_view.dart';
@@ -12,7 +12,7 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomeView(),
+      home: const HomeView(),
       routes: {
         "/login/": (context) => const LoginView(),
         "/register/": (context) => Register_View(),
@@ -28,8 +28,8 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      // future: Firebase.initializeApp(),
-      future: FirebaseAuthService.initialize(),
+      future: Firebase.initializeApp(),
+      // future: FirebaseAuthService.initialize(),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
